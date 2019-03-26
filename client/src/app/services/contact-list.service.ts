1edf844835a4211d49f5ae4d96892022d37c2f64
Dongwan Kim <zeumes12@gmail.com>
2019-03-25 22:21:15 -0400

    Added route guard for contact-list

--- client/src/app/services/contact-list.service.ts
+++ client/src/app/services/contact-list.service.ts
@@ -9,9 +9,10 @@ import { User } from '../models/user';
   providedIn: 'root'
 })
 export class ContactListService {
-  private user:User;
+  private user: User;
+  private authToken: any = null;
 
-  private endpoint = "http://localhost:3000/api/contact-list/";
+  private endpoint = 'http://localhost:3000/api/contact-list/';
   private httpOptions = {
     headers: new HttpHeaders({
       'Content-Type': 'application/json',
@@ -19,25 +20,39 @@ export class ContactListService {
       'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept'
     })
   };
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {
+    this.user = new User();
 
-  public getList(): Observable<any>{
-    return this.http.get<any>(this.endpoint,this.httpOptions);
+   }
+
+  public getList(): Observable<any> {
+    this.loadToken();
+    return this.http.get<any>(this.endpoint, this.httpOptions);
   }
 
-  public getContact(contact:Contact):Observable<any>{
+  public getContact(contact: Contact): Observable<any> {
+    this.loadToken();
     return this.http.get<any>(this.endpoint + 'edit/' + contact._id, this.httpOptions);
   }
 
-  public addContact(contact:Contact):Observable<any>{
+  public addContact(contact: Contact): Observable<any> {
+    this.loadToken();
     return this.http.post<any>(this.endpoint + 'add', contact, this.httpOptions);
   }
 
-  public editContact(contact:Contact):Observable<any>{
+  public editContact(contact: Contact): Observable<any> {
+    this.loadToken();
     return this.http.post<any>(this.endpoint + 'edit/' + contact._id, contact, this.httpOptions);
   }
 
-  public deleteContact(contact:Contact):Observable<any>{
+  public deleteContact(contact: Contact): Observable<any> {
+    this.loadToken();
     return this.http.get<any>(this.endpoint + 'delete/' + contact._id, this.httpOptions);
+  }
+
+  private loadToken() {
+    const token = localStorage.getItem('id_token');
+    this.authToken = token;
+    this.httpOptions.headers = this.httpOptions.headers.set('Authorization', this.authToken);
   }
 }
